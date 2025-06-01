2290637c573a021b489c78cc46e0791642321d30
Katie P <personal@justkate.me>
2025-06-01 02:35:01 +0200

    Added custom trans flag themed statusline to nvim

--- config/nvim/lua/plugins/flexoki.lua
+++ config/nvim/lua/plugins/flexoki.lua
@@ -2,8 +2,25 @@ return {
 	"kepano/flexoki-neovim",
 	dev      = true,
 	priority = 1000,
-	config   = function ()
-		vim.cmd.colorscheme("flexoki-dark")
-		--vim.cmd.highlight("Folded guibg=NONE guifg=darkorange")
-	end
+
+	---@module "flexoki"
+	---@type FlexokiOptions
+	opts = {
+		auto_apply = true,
+		highlight_groups = {
+			["TransWhiteBg"] = { bg = "#EEEEEE", fg = "#100F0F" },
+			["TransWhiteFg"] = { fg = "#EEEEEE" },
+
+			["TransPinkBg"] = { bg = "#F5A9B8", fg = "#100F0F" },
+			["TransPinkFg"] = { fg = "#F5A9B8" },
+
+			["TransBlueBg"] = { bg = "#5BCEFA", fg = "#100F0F" },
+			["TransBlueFg"] = { fg = "#5BCEFA" },
+
+			["TransWP"] = { bg = "#EEEEEE", fg = "#F5A9B8" },
+			["TransPB"] = { bg = "#F5A9B8", fg = "#5BCEFA" },
+			["TransBP"] = { bg = "#5BCEFA", fg = "#F5A9B8" },
+			["TransPW"] = { bg = "#F5A9B8", fg = "#EEEEEE" }
+		}
+	}
 }
--- config/nvim/lua/plugins/mini-nvim.lua
+++ config/nvim/lua/plugins/mini-nvim.lua
@@ -22,6 +22,46 @@ return {
 				update_n_lines = '<leader>sn', -- Update `n_lines`
 			}
 		})
+		require('mini.git').setup()
 		require('mini.indentscope').setup()
+		MiniStatusline = require('mini.statusline')
+
+		MiniStatusline.setup({
+			content = {
+				active = function()
+					local mode          = MiniStatusline.section_mode({ trunc_width = 120 })
+					local git           = MiniStatusline.section_git({ trunc_width = 40 })
+					local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+					local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+					local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+					local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+					local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+					local location      = MiniStatusline.section_location({ trunc_width = 75 })
+					local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+					local devInfo = MiniStatusline.combine_groups({
+						{  strings = { git, diff, diagnostics, lsp } },
+					})
+
+					if #devInfo == 0 then
+						devInfo = " "
+					end
+
+					return MiniStatusline.combine_groups({
+						{ hl = 'TransBlueBg',  strings = { mode } },
+						'%#TransPB#',
+						{ hl = 'TransPinkBg',  strings = { devInfo } },
+						'%#TransWP#',
+						'%<', -- Mark general truncate point
+						{ hl = 'TransWhiteBg', strings = { filename } },
+						'%=', -- End left alignment
+						'%#TransWP#',
+						{ hl = 'TransPinkBg',  strings = { fileinfo } },
+						'%#TransPB#',
+						{ hl = 'TransBlueBg',  strings = { search, location } },
+					})
+				end
+			}
+		})
 	end
 }
